ac93f5c0800b09b9f288a57aa9958e435288e071
Suraj K J <93546991+srj-kj@users.noreply.github.com>
2022-12-05 13:22:41 +0530

    commited

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,21 @@
 const mongoClient=require('mongodb').MongoClient
-// MongoClient.connect('mongodb://127.0.0.1:27017', function(err, client) {
-//     if(err)
-//         console.log('error');
-//     else
-//      client.db('Fincops').collection('users').insertOne(req.body)
-// })
+
 
 const state={
     db:null
 }
-module.exports.connect=function(done){
-    // const url='mongodb://127.0.0.1:27017'
+
+module.exports.connect = function(done){
+    // const url = 'mongodb://127.0.0.1:27017'
     const url ='mongodb+srv://surajkj:Suraj222317@cluster0.p1rcydn.mongodb.net/test'
     const dbname= 'picykle'
     mongoClient.connect(url,(err,data)=>{
-        if(err) return done(err)
+        if(err){ 
+            console.log(err);
+            return done(err)}
+            else{
+                console.log("cconnected");
+            }
         state.db=data.db(dbname)
     })
     done()
